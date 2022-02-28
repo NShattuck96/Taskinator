ec96202b126ee30137ed85fb7aa216570d654fd8
Nathan Shattuck <Nshattuck96@gmail.com>
2022-02-28 16:39:09 -0500

    tasktype inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,15 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-
     event.preventDefault();
-
+    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("selct[name='task-type']").value;
+    console.log(taskTypeInput);
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new Task.";
+    listItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(listItemEl);
 };
 
